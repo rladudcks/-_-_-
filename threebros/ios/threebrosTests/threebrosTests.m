7282fa64ce56106591686045d11f2287059639ae
yammong <emaehol28@gmail.com>
2021-03-21 15:39:47 +0900

    Update Sample Project

--- threebros/ios/threebrosTests/threebrosTests.m
+++ threebros/ios/threebrosTests/threebrosTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface threebrosTests : XCTestCase
+@interface ThreeBrosTests : XCTestCase
 
 @end
 
-@implementation threebrosTests
+@implementation ThreeBrosTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
