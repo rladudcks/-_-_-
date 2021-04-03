0171784e36c90a60b4528b3551c4ab7af3a88071
yammong <emaehol28@gmail.com>
2021-04-03 17:37:09 +0900

    Upload Image Picker Sample Code

--- threebros/App.js
+++ threebros/App.js
@@ -1,26 +1,173 @@
-import React from 'react';
+import React, { Fragment, Component } from 'react';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
 import {
+  SafeAreaView,
   StyleSheet,
-  Text,
   View,
+  Text,
+  StatusBar,
+  Image,
+  Button,
+  Dimensions,
+  TouchableOpacity
 } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello World!</Text>
-    </View>
-  );
-}
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const options = {
+  title: 'Select Avatar',
+  customButtons: [{ name: 'fb', title: 'Choose Photo from Facebook' }],
+  storageOptions: {
+    skipBackup: true,
+    path: 'images',
+  },
+};
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      filepath: {
+        data: '',
+        uri: ''
+      },
+      fileData: '',
+      fileUri: ''
+    }
+  }
+
+  loadFromImageLibrary = () => {
+    let options = {
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+
+      mediaType: 'photo',
+    };
+    launchImageLibrary(options, (response) => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+        alert(response.customButton);
+      } else {
+        const source = { uri: response.uri };
+        console.log('response', JSON.stringify(response));
+        this.setState({
+          filePath: response,
+          fileData: response.data,
+          fileUri: response.uri
+        });
+      }
+    });
+
+  }
+
+  renderFileData() {
+    if (this.state.fileData) {
+      return <Image source={{ uri: 'data:image/jpeg;base64,' + this.state.fileData }}
+        style={styles.images}
+      />
+    } else {
+      return <Image
+        style={styles.images}
+      />
+    }
+  }
+
+  renderFileUri() {
+    if (this.state.fileUri) {
+      return <Image
+        source={{ uri: this.state.fileUri }}
+        style={styles.images}
+      />
+    } else {
+      return <Image
+        style={styles.images}
+      />
+    }
+  }
+  render() {
+    return (
+      <Fragment>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView>
+          <View style={styles.body}>
+            <Text style={{textAlign:'center',fontSize:20,paddingBottom:10}} >Pick Images from Gallery</Text>
+            <View style={styles.ImageSections}>
+              <View>
+                {this.renderFileUri()}
+                <Text style={{textAlign:'center'}}>File Uri</Text>
+              </View>
+            </View>
+
+            <View style={styles.btnParentSection}>
+              <TouchableOpacity onPress={this.loadFromImageLibrary} style={styles.btnSection}  >
+                <Text style={styles.btnText}>Select Image From Gallery</Text>
+              </TouchableOpacity>
+            </View>
+
+          </View>
+        </SafeAreaView>
+      </Fragment>
+    );
+  }
+};
 
 const styles = StyleSheet.create({
-  container : {
-    flex : 1,
-    backgroundColor : '#fff',
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+
+  body: {
+    backgroundColor: Colors.white,
+    justifyContent: 'center',
+    borderColor: 'black',
+    borderWidth: 1,
+    height: Dimensions.get('screen').height - 20,
+    width: Dimensions.get('screen').width
+  },
+  ImageSections: {
+    display: 'flex',
+    flexDirection: 'row',
+    paddingHorizontal: 8,
+    paddingVertical: 8,
+    justifyContent: 'center'
+  },
+  images: {
+    width: 150,
+    height: 150,
+    borderColor: 'black',
+    borderWidth: 1,
+    marginHorizontal: 3
+  },
+  btnParentSection: {
+    alignItems: 'center',
+    marginTop:10
+  },
+  btnSection: {
+    width: 225,
+    height: 50,
+    backgroundColor: '#DCDCDC',
     alignItems: 'center',
     justifyContent: 'center',
+    borderRadius: 3,
+    marginBottom:10
   },
-  title : {
-    fontSize : 20
+  btnText: {
+    textAlign: 'center',
+    color: 'gray',
+    fontSize: 14,
+    fontWeight:'bold'
   }
 });
